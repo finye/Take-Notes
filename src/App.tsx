35abca2fcf6bb9efa78b215eaf46c1f94303f29f
finye <35150952+finye@users.noreply.github.com>
2023-06-05 20:52:59 +0300

    Adds routes (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
 
+import {Routes, Route, Navigate} from "react-router-dom"
+import "bootstrap/dist/css/bootstrap.min.css"
+import {Container} from "react-bootstrap"
 function App() {
 
-  return <h1>HI</h1>
+  return (
+  <Container className="my-4">
+  <Routes>
+    <Route path="/" element={<h1>Home</h1>}/>
+    <Route path="/new" element={<h1>NewNote</h1>}/>
+    <Route path="/:id" >
+      <Route index element={<h1>ShowNote</h1>}/>
+      <Route path="edit" element={<h1>EditNote</h1>} />
+    </Route>
+    <Route path="*" element={<Navigate to ="/" /> }/>
+  </Routes>
+  </Container>
+)
 }
 export default App
