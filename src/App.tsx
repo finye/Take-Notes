5e1a9ebddbe863cf5ed652db90435fe7de4746c7
finye <finnantiku@gmail.com>
2023-06-12 15:40:18 +0300

    creates a note form component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@
 import {Routes, Route, Navigate} from "react-router-dom"
 import "bootstrap/dist/css/bootstrap.min.css"
 import {Container} from "react-bootstrap"
+import { NewNote } from "./NewNote"
+
 function App() {
 
   return (
   <Container className="my-4">
   <Routes>
     <Route path="/" element={<h1>Home</h1>}/>
-    <Route path="/new" element={<h1>NewNote</h1>}/>
+    <Route path="/new" element={<NewNote/>}/>
     <Route path="/:id" >
       <Route index element={<h1>ShowNote</h1>}/>
       <Route path="edit" element={<h1>EditNote</h1>} />
