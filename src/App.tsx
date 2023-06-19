054cc6e86e986487eaadfc18067011988f22f93c
finye <finnantiku@gmail.com>
2023-06-19 14:22:17 +0300

    sets title and markdown ref for new notes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,36 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import { NewNote } from "./NewNote";
 
-import {Routes, Route, Navigate} from "react-router-dom"
-import "bootstrap/dist/css/bootstrap.min.css"
-import {Container} from "react-bootstrap"
-import { NewNote } from "./NewNote"
+export type Note = {
+  id: string;
+} & NoteData;
+
+export type NoteData = {
+  title: string;
+  markdown: string;
+  tags: Tag[];
+};
+
+export type Tag = {
+  id: string;
+  label: string;
+};
 
 function App() {
-
   return (
-  <Container className="my-4">
-  <Routes>
-    <Route path="/" element={<h1>Home</h1>}/>
-    <Route path="/new" element={<NewNote/>}/>
-    <Route path="/:id" >
-      <Route index element={<h1>ShowNote</h1>}/>
-      <Route path="edit" element={<h1>EditNote</h1>} />
-    </Route>
-    <Route path="*" element={<Navigate to ="/" /> }/>
-  </Routes>
-  </Container>
-)
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/new" element={<NewNote />} />
+        <Route path="/:id">
+          <Route index element={<h1>ShowNote</h1>} />
+          <Route path="edit" element={<h1>EditNote</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
+  );
 }
-export default App
+export default App;
--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,16 +1,34 @@
+import { FormEvent, useRef } from "react";
 import { Button, Col, Form, Row, Stack } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import CreatableReactSelect from "react-select/creatable";
+import { NoteData } from "./App";
 
-export function NoteForm() {
+type NoteFormProps = {
+  onSubmit: (data: NoteData) => void;
+};
+
+export function NoteForm({ onSubmit }: NoteFormProps) {
+  const titleRef = useRef<HTMLInputElement>(null);
+  const markdownRef = useRef<HTMLTextAreaElement>(null);
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    onSubmit({
+      title: titleRef.current!.value,
+      markdown: markdownRef.current!.value,
+      tags: [],
+    });
+  }
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Stack gap={5}>
         <Row>
           <Col>
             <Form.Group controlId="title">
               <Form.Label>Title</Form.Label>
-              <Form.Control required />
+              <Form.Control ref={titleRef} required />
             </Form.Group>
           </Col>
           <Col>
@@ -22,7 +40,7 @@ export function NoteForm() {
         </Row>
         <Form.Group>
           <Form.Label>Body</Form.Label>
-          <Form.Control required as="textarea" rows={15} />
+          <Form.Control required as="textarea" ref={markdownRef} rows={15} />
         </Form.Group>
         <Stack direction="horizontal" gap={2} className="justify-content-end">
           <Button type="submit" variant="primary">
